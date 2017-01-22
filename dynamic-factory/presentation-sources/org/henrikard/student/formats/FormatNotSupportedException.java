4b806c01f08a994ce8563a12f50e8fae6bbbd5a3
Rikard Fröberg <rikard@morus.se>
2017-01-22 15:33:43 +0100

    Sources for the lecture on using a factory to produce either json or xml - dynamically

---
+++ dynamic-factory/presentation-sources/org/henrikard/student/formats/FormatNotSupportedException.java
@@ -0,0 +1,7 @@
+package org.henrikard.student.formats;
+
+public class FormatNotSupportedException extends Exception{
+    public FormatNotSupportedException(String msg){
+        super(msg);
+    }
+}
