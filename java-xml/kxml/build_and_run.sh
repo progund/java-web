2f7b1cc0ccac458ce0446f180ae449fec1c1db19
Rikard Fröberg <rikard@morus.se>
2017-08-10 00:47:38 +0200

    New version of build files

--- java-xml/kxml/build_and_run.sh
+++ java-xml/kxml/build_and_run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+echo "Simple example with users.xml"
 javac -cp .:kxml2-2.3.0.jar XmlPullExample.java && java -cp .:kxml2-2.3.0.jar XmlPullExample
---
+++ java-xml/kxml/build_and_run_alco.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Example with sortiment.xml"
+javac -cp .:kxml2-2.3.0.jar XmlPullExampleBolaget.java && java -cp .:kxml2-2.3.0.jar XmlPullExampleBolaget
