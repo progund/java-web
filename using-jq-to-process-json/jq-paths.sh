610c44ca8b1332cdfe6f80f24440c18ea1bbd616
Rikard Fröberg <rikard@morus.se>
2018-12-04 12:04:58 +0100

    handle keys with dash - surround with quotes

--- using-jq-to-process-json/jq-paths.sh
+++ using-jq-to-process-json/jq-paths.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cat | jq 'if type == "array" then .[] else . end' | jq -c 'path(..)' | tr -d '[\[\]"]' | tr ',' '.' | sed -e 's/\(^[a-z_.-]*\)\([0-9]\+\)\(.*\)/.\1[\2\]\3/g'|sed -e 's/\.\[/[/g'
+#cat | jq 'if type == "array" then .[] else . end' | jq -c 'path(..)' | tr -d '[\[\]"]' | tr ',' '.' | sed -e 's/\(^[a-z_.-]*\)\(\.[0-9]\+\)\(.*\)/.\1[\2\]\3/g'|sed -e 's/\.\[/[/g' | sed -e 's/\[\./[/g' | sed -e 's/\(^[^.]\)/.\1/g' | sed -e 's/\([a-z]\+-[a-z]\+\)/"\1"/g'
+cat | jq 'if type == "array" then .[] else . end' | jq -c 'path(..)' | tr -d '[\[\]"]' | tr ',' '.' | sed -e 's/\(^[a-z_.-]*\)\(\.[0-9]\+\)\(.*\)/.\1[\2\]\3/g;s/\.\[/[/g;s/\[\./[/g;s/\(^[^.]\)/.\1/g;s/\([a-z]\+-[a-z]\+\)/"\1"/g'
 
